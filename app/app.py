7457384c6e44f4edb50ed35926da3f8907e4cb01
david roper <davidroper321@gmail.com>
2021-01-31 17:12:19 -0500

    Added comment to app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 #app folder for our instagram app, will contain app routes for traversing
 #through the application
 
+#code to start application
 if __name__ == "__main__":
     #app.debug == true
     app.run()
