c6f0446dec81393673e141b0362d16048140c3c3
Omarelk09 <65197387+Omarelk09@users.noreply.github.com>
2021-02-15 14:42:30 -0500

    setting up flask and templates

--- app/app.py
+++ app/app.py
@@ -2,6 +2,15 @@
 #through the application
 
 #code to start application
-if __name__ == "__main__":
-    #app.debug == true
-    app.run()
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/Timeline")
+def home():
+    return render_template('timeline.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
