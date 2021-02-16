791c2715c9223e102c500688768c21942855b777
gianfrancodumoulinbertucci <francodumoulin123@gmail.com>
2021-02-15 23:53:57 -0500

    added backend for reading posts from posts.json #20

--- app/app.py
+++ app/app.py
@@ -2,14 +2,20 @@
 #through the application
 
 #code to start application
+from json import load
 from flask import Flask, render_template
 app = Flask(__name__)
+
+
+posts = ''
+with open('static/posts.json', 'r') as read_file:
+    posts = load(read_file)
 
 @app.route("/")
 @app.route("/timeline")
 def home():
-    return render_template('timeline.html')
+    return render_template('timeline.html', posts=posts)
 
 
-if __name__ == __'main__':
+if __name__ == '__main__':
     app.run(debug=True)
