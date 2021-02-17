33e517f138cc89896218036a4ca1ee94521e23b9
MAXWELL LEE <MAXWELL.LEE@sympatico.ca>
2021-02-17 15:11:31 -0500

    Added route for posting images in app.py (back-end) #20

--- app/app.py
+++ app/app.py
@@ -2,13 +2,16 @@
 #through the application
 
 #code to start application
-from json import load
-from flask import Flask, render_template
+from datetime import datetime
+from json import load, dump
+from flask import Flask, render_template, request, url_for, redirect
+from os import path
+
 app = Flask(__name__)
 
 
 posts = ''
-with open('static/posts.json', 'r') as read_file:
+with open('app/static/posts.json', 'r') as read_file:
     posts = load(read_file)
 
 @app.route("/")
@@ -20,7 +23,47 @@ def home():
 @app.route("/posting" )
 def post():
     return render_template('posting.html', posts=posts)
+
+
+@app.route("/posting", methods=['POST'])
+def image_post():
+    poster_name = "Default"
+    date = datetime.today().strftime("%d/%m/%Y")
     
+    img_file = request.files['file']
+    description = request.form['description']
+
+    if img_file.filename == "":
+        print("No file selected")
+        return redirect(url_for('post'))
+
+    file_path = path.join(app.root_path, 'static/images', img_file.filename)
+    img_file.save(file_path)
+
+    new_post = {}
+    new_post['author']      = poster_name
+    new_post['description'] = description
+    new_post['image']       = img_file.filename
+    new_post['date_posted'] = date
+
+    with open('app/static/posts.json', 'w') as all_posts:
+        posts.insert(0, new_post)
+        dump(posts, all_posts)
+
+    return redirect(url_for('home'))
+   
+
+
+    
+
+
+
+    
+    
+
+    
+
+
     
 
 
