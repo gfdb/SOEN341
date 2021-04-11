dccb96c3f552fab3ce547a53115a2f49a2ee65af
Omarelk09 <65197387+Omarelk09@users.noreply.github.com>
2021-04-11 12:15:15 -0400

    test sample #57

--- test_pytest.py
+++ test_pytest.py
@@ -1,6 +1,10 @@
-def test_lang():
-    assert sum([1, 2, 3]) == 6, "Should be 6"
+import unittest
+
+
+class TestInsta(unittest.TestCase):
+
+    def test_login(self):
+        assert sum([1, 2, 3]) == 6, "Should be 6"
 
 if __name__ == "__main__":
-    test_lang()
-    print("Everything passed")
+    unittest.main()
